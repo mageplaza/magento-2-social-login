c51c557bfbb2c814e98c87f914e2a7238c6ced96
btn114 <nghiabt@mageplaza.com>
2018-01-16 16:00:54 +0700

    fix bug wrong template

--- Block/Form/Register.php
+++ Block/Form/Register.php
@@ -29,7 +29,7 @@ namespace Mageplaza\SocialLogin\Block\Form;
 class Register extends \Magento\Customer\Block\Form\Register
 {
 	
-	protected $_template='Mageplaza_SocialLoginPro::popup/form/create.phtml';
+	protected $_template='Mageplaza_SocialLogin::popup/form/create.phtml';
 	
     /**
      * @return $this
