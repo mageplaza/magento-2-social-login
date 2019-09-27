0395cc92a6e394ee10a3b61706ae5d71ab5bbd2d
trunghieu <hieudt@mageplaza.com>
2019-09-27 17:29:08 +0700

    Change labels

--- view/frontend/web/js/provider.js
+++ view/frontend/web/js/provider.js
@@ -11,7 +11,22 @@
  *
  * Do not edit or add to this file if you wish to upgrade this extension to newer
  * version in the future.
- *
+ *What I found in my quick research is:
+
+
+In magento 1 it was quite easy to get the admin session on frontend since magento had no restrictions when it expose the admin session cookie.
+
+
+
+But in magento2, the admin cookie will get exposed only for the admin path, for example /admin, because of this, you won't be able to access the admin session on frontend.
+
+
+
+But as an alternate solution, you can try:
+
+Build an admin route & a controller for you module
+When frontend loads, send an ajax request to the admin controller and check the auth for isLoggedIn()
+Initialize your plugin / related code via JS through ajax callback.
  * @category    Mageplaza
  * @package     Mageplaza_SocialLogin
  * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
