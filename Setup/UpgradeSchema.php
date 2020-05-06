256efe56f56587bd9e5f84ad6f775f81ff4c1bcf
vinhlt <vinhlt@mageplaza.com>
2020-05-06 10:45:12 +0700

    Clean code

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -42,49 +42,49 @@ class UpgradeSchema implements UpgradeSchemaInterface
         $installer->startSetup();
         $connection = $installer->getConnection();
         $tableName  = $setup->getTable('mageplaza_social_customer');
-        if (version_compare($context->getVersion(), '1.2.0', '<')
-            && $connection->tableColumnExists($tableName, 'social_created_at') === false
-        ) {
-            $connection->addColumn(
-                $tableName,
-                'social_created_at',
-                [
-                    'type'    => Table::TYPE_TIMESTAMP,
-                    'comment' => 'Social Created At',
-                ]
-            );
-        if ($connection->tableColumnExists($tableName, 'user_id') === false) {
-            $connection->addColumn(
-                $tableName,
-                'user_id',
-                [
-                    'type'     => Table::TYPE_INTEGER,
-                    'nullable' => true,
-                    'unsigned' => true,
-                    'comment'  => 'User Id',
-                ]
-            );
-            $connection->addForeignKey(
-                $installer->getFkName('mageplaza_social_customer', 'user_id', 'admin_user', 'user_id'),
-                $tableName,
-                'user_id',
-                $installer->getTable('admin_user'),
-                'user_id',
-                Table::ACTION_CASCADE
-            );
-        }
-        if ($connection->tableColumnExists($tableName, 'status') === false) {
-            $connection->addColumn(
-                $tableName,
-                'status',
-                [
-                    'type'     => Table::TYPE_TEXT,
-                    'nullable' => true,
-                    'length'   => 255,
-                    'comment'  => 'Status',
-                ]
-            );
-        }
+        if (version_compare($context->getVersion(), '1.2.0', '<')) {
+            if ($connection->tableColumnExists($tableName, 'social_created_at') === false) {
+                $connection->addColumn(
+                    $tableName,
+                    'social_created_at',
+                    [
+                        'type'    => Table::TYPE_TIMESTAMP,
+                        'comment' => 'Social Created At',
+                    ]
+                );
+            }
+            if ($connection->tableColumnExists($tableName, 'user_id') === false) {
+                $connection->addColumn(
+                    $tableName,
+                    'user_id',
+                    [
+                        'type'     => Table::TYPE_INTEGER,
+                        'nullable' => true,
+                        'unsigned' => true,
+                        'comment'  => 'User Id',
+                    ]
+                );
+                $connection->addForeignKey(
+                    $installer->getFkName('mageplaza_social_customer', 'user_id', 'admin_user', 'user_id'),
+                    $tableName,
+                    'user_id',
+                    $installer->getTable('admin_user'),
+                    'user_id',
+                    Table::ACTION_CASCADE
+                );
+            }
+            if ($connection->tableColumnExists($tableName, 'status') === false) {
+                $connection->addColumn(
+                    $tableName,
+                    'status',
+                    [
+                        'type'     => Table::TYPE_TEXT,
+                        'nullable' => true,
+                        'length'   => 255,
+                        'comment'  => 'Status',
+                    ]
+                );
+            }
         }
         $installer->endSetup();
     }
