4da72f0ec5c8f80ece7d1928d72985de09cdcce9
Hervé H <nenes25@users.noreply.github.com>
2021-11-30 17:15:38 +0100

    Replace private function by protected

--- Model/Social.php
+++ Model/Social.php
@@ -260,7 +260,7 @@ class Social extends AbstractModel
      *
      * @return EmailNotificationInterface
      */
-    private function getEmailNotification()
+    protected function getEmailNotification()
     {
         return ObjectManager::getInstance()->get(EmailNotificationInterface::class);
     }
