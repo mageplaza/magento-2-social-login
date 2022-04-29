bb910c3b6c91746e225a8a18ce1a37ab55bbd11e
justin <khietnv@mageplaza.com>
2022-04-29 09:49:58 +0700

    Lỗi khi sign in với Vkontakte và Lỗi yêu cầu nhập password sau các lần sign in

--- Controller/Social/Login.php
+++ Controller/Social/Login.php
@@ -90,7 +90,8 @@ class Login extends AbstractSocial
             }
 
             $customer = $this->createCustomerProcess($userProfile, $type);
-        } elseif ($this->apiHelper->isCheckMode()) {
+        }
+        if ($this->apiHelper->isCheckMode()) {
             if ($customerData->getData('password_hash') === null) {
                 $userProfile->hash = '';
                 $this->session->setUserProfile($userProfile);
@@ -104,18 +105,6 @@ class Login extends AbstractSocial
                     )
                 );
             }
-
-            $userProfile->hash = $customerData->getData('password_hash');
-            $this->session->setUserProfile($userProfile);
-
-            return $this->_appendJs(
-                sprintf(
-                    "<script>window.close();window.opener.fakeEmailCallback('%s','%s','%s');</script>",
-                    $type,
-                    $userProfile->firstName,
-                    $userProfile->lastName
-                )
-            );
         }
         $this->refresh($customer);
 
