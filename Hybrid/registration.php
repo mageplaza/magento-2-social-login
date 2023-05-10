2abc34809e8e087e2a937057349f637353daa4ba
justin <khietnv@mageplaza.com>
2023-05-10 16:35:26 +0700

    Update Lib

--- Hybrid/registration.php
+++ Hybrid/registration.php
@@ -15,37 +15,10 @@
  *
  * @category    Mageplaza
  * @package     Mageplaza_SocialLoginLib
- * @copyright   Copyright (c) 2016 Mageplaza (http://www.mageplaza.com/)
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
  * @license     https://www.mageplaza.com/LICENSE.txt
  */
-class Hybrid_Autoload
-{
-	public static function autoload ($class) {
-		// project-specific namespace prefix
-		$prefix = 'Hybrid_';
+require_once dirname(__DIR__) . '/registration.php';
 
-		// base directory for the namespace prefix
-		$baseDir = __DIR__ . '/';
-
-		// does the class use the namespace prefix?
-		$len = strlen($prefix);
-		if (strncmp($prefix, $class, $len) !== 0) {
-			// no, move to the next registered autoloader
-			return;
-		}
-
-		// get the relative class name
-		$relativeClass = substr($class, $len);
-
-		// replace the namespace prefix with the base directory, replace namespace
-		// separators with directory separators in the relative class name, append
-		// with .php
-		$file = rtrim($baseDir, '/') . '/' . str_replace('\\', '/', $relativeClass) . '.php';
-
-		// if the file exists, require it
-		if (file_exists($file)) {
-			require $file;
-		}
-	}
-}
-spl_autoload_register(['Hybrid_Autoload', 'autoload']);
+$loader = new MageplazaSplClassLoader('Hybridauth', realpath(__DIR__ . '/src/'));
+$loader->register();
